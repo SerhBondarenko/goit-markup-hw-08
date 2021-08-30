fa6490121dc39637396d0dd922f3744a2567b76b
SerhBondarenko <serhii.bondarenk@gmail.com>
2021-08-30 19:42:51 +0300

    5

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,24 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+
+
+
+/* модалка для телефона
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();*/
